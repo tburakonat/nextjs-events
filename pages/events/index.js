34f92acf863f2a6ac0dab81654fc672b078022a6
tburakonat <tburakonat@gmail.com>
2022-12-15 21:17:07 +0100

    finish up all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+
 export default function AllEventsPage() {
+	const events = getAllEvents();
+
 	return (
 		<div>
 			<h1>All Events</h1>
+			<EventList items={events} />
 		</div>
 	);
 }
