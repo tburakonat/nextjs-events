bb2736e6a81d9f8779bed5b76e2d2c4ee30387e2
tburakonat <tburakonat@gmail.com>
2022-12-15 20:27:49 +0100

    style event details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,27 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/EventSummary';
+import EventLogistics from '../../components/event-detail/EventLogistics';
+import EventContent from '../../components/event-detail/EventContent';
+
 export default function EventDetailPage() {
+	const router = useRouter();
+	const { eventId } = router.query;
+
+	const event = getEventById(eventId);
+
+	if (!event) {
+		return <p>No event Found</p>;
+	}
+
 	return (
-		<div>
-			<h1>Event Detail</h1>
-		</div>
+		<React.Fragment>
+			<EventSummary title={event.title} />
+			<EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</React.Fragment>
 	);
 }
