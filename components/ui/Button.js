98e02e5a9a26cf1e9412ddac9de6b4023cd44250
tburakonat <tburakonat@gmail.com>
2022-12-15 21:39:58 +0100

    add filter form to home page

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -2,9 +2,17 @@ import Link from 'next/link';
 import styles from './Button.module.css';
 
 export default function Button(props) {
+	if (props.link) {
+		return (
+			<Link href={props.link} className={styles.btn}>
+				{props.children}
+			</Link>
+		);
+	}
+
 	return (
-		<Link href={props.link} className={styles.btn}>
+		<button className={styles.btn} onClick={props.onClick}>
 			{props.children}
-		</Link>
+		</button>
 	);
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,23 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
+import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function AllEventsPage() {
+	const router = useRouter();
 	const events = getAllEvents();
 
+	function findEventsHandler(year, month) {
+		const fullPath = `/events/${year}/${month}`;
+		router.push(fullPath);
+	}
+
 	return (
-		<div>
+		<React.Fragment>
 			<h1>All Events</h1>
+			<EventsSearch onSearch={findEventsHandler} />
 			<EventList items={events} />
-		</div>
+		</React.Fragment>
 	);
 }
