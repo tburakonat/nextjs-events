3d7ad348341953ab8934077410408d89da1b5f80
tburakonat <tburakonat@gmail.com>
2022-12-16 19:10:31 +0100

    pre-render homepage and fetch data from database

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
 import EventList from '../components/events/EventList';
-import { getFeaturedEvents } from '../dummy-data';
+// import { getFeaturedEvents } from '../dummy-data';
 
-export default function HomePage() {
-	const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
+	// const featuredEvents = getFeaturedEvents();
 
 	return (
 		<div>
-			<EventList items={featuredEvents} />
+			<EventList items={props.featuredEvents} />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const response = await fetch(
+		'https://nextjs-events-ad399-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+	);
+	const data = await response.json();
+
+	let events = [];
+
+	for (let key in data) {
+		events.push({ ...data[key], id: key });
+	}
+
+	const featuredEvents = events.filter(event => event.isFeatured === true);
+
+	return {
+		props: {
+			featuredEvents,
+		},
+	};
 }
